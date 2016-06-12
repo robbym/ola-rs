a98b7f23c10028089cbc0e1b8031d46308476f6d
Robby Madruga <Robby Madruga>
2016-06-11 17:29:41 -0700

    Moved tests folder to correct directory. Added some RpcController code

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,36 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
+pub struct RpcSession;
+pub struct RpcController {
+    session: Option<RpcSession>,
+    failed: bool,
+    error_str: String,
+}
+
+impl RpcController {
+    pub fn new() -> RpcController {
+        RpcController{
+            session: None,
+            failed: false,
+            error_str: String::new()
+        }
+    }
+
+    pub fn reset(&mut self) {
+
+    }
+
+    pub fn failed(&self) -> bool {
+        false
+    }
+
+    pub fn error_str(&self) -> &String {
+        &self.error_str
+    }
+
+    pub fn set_failed(&mut self, reason: String) {
+
+    }
+
+    pub fn session(&self) -> RpcSession {
+        RpcSession
     }
 }
---
+++ tests/lib.rs
@@ -0,0 +1,7 @@
+extern crate ola;
+use ola::RpcController;
+
+#[test]
+fn RpcController_SetFailed() {
+    let ctrl = RpcController::new();
+}
