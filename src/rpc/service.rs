2ad0a4ab334b24624d21529ecfc048a97ad07fb2
Robby Madruga <robbymadruga@gmail.com>
2016-06-11 20:17:51 -0700

    Implemented RpcService

--- src/rpc/service.rs
+++ src/rpc/service.rs
@@ -1,1 +1,13 @@
-pub struct RpcService;
+use protobuf::descriptor::MethodDescriptorProto;
+use protobuf::core::Message;
+
+use rpc::RpcController;
+
+pub type CompletionCallback = Fn();
+
+pub trait RpcService {
+    fn descriptor<'a>() -> &'a MethodDescriptorProto;
+    fn call_method(method: &MethodDescriptorProto, controller: &mut RpcController, request: &Message, response: &mut Message, done: CompletionCallback);
+    fn request_proto<'a>(method: &MethodDescriptorProto) -> &'a Message;
+    fn response_proto<'a>(method: &MethodDescriptorProto) -> &'a Message;
+}
